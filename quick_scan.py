b71429bcce7cd69b07a09f04e8fa25c72ea439d7
BradenBug <bradenwicker@gmail.com>
2018-10-06 12:27:17 -0700

    Updated parse_json()

--- quick_scan.py
+++ quick_scan.py
@@ -1,8 +1,15 @@
-import json
+import json, sys
 
 def parse_json():
+   """Parses the .json file given as the first command line argument.
+   If no .json file is specified, defaults to "configs.json".
+   If the specified .json file is found, returns a dict of the contents.
+   Otherwise, raises a FileNotFoundError."""
    try:
-      with open("configs.json", "r") as read_file:
-         return json.load(read_file)
+      if (len(sys.argv) == 1):
+         read_file = open("configs.json", "r")
+      else:
+         read_file =  open(sys.argv[1], "r")
+      return json.load(read_file)
    except FileNotFoundError:
       raise
