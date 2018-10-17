3bd7aa7d7aa76e4ba546a92f7c93c855fc08a7a9
tianatonnu <tianattonnu@gmail.com>
2018-10-16 18:53:09 -0700

    xbee callback, json message parsing (#23)
    quick_scan parses configs file, sets up xBee radio device, and starts autonomy thread. autonomy.py contains callback function that changes globals according to messages from GCS, sets search area, and sends back acknowledgment message to sender(GCS)

--- quick_scan.py
+++ quick_scan.py
@@ -1,15 +1,59 @@
 import json, sys
+from digi.xbee.devices import XBeeDevice
+from autonomy import autonomy
 
-def parse_json():
-   """Parses the .json file given as the first command line argument.
-   If no .json file is specified, defaults to "configs.json".
-   If the specified .json file is found, returns a dict of the contents.
-   Otherwise, raises a FileNotFoundError."""
-   try:
-      if (len(sys.argv) == 1):
-         read_file = open("configs.json", "r")
-      else:
-         read_file =  open(sys.argv[1], "r")
-      return json.load(read_file)
-   except FileNotFoundError:
-      raise
+
+# Instantiates xBee device object
+def setup_xbee(configs):
+    xbee = None
+
+    # Only set-up xBee radio if not simulated
+    if configs["comms_simulated"] == False:
+        os = configs["os"]
+        port_name = ""
+
+        if os == "Mac":
+            port_name = configs["xbee_port_name"]
+        elif os == "Linux":
+            port_name = "/dev/ttyUSB0"
+        # TODO: figure out windows port name
+        elif os == "Windows":
+            port_name = "COMS1"
+
+        # Instantiate XBee device object.
+        xbee = XBeeDevice(port_name, 57600)
+        xbee.open()
+
+    return xbee
+
+
+def parse_json(argv):
+    """Parses the .json file given as the first command line argument.
+    If no .json file is specified, defaults to "configs.json".
+    If the specified .json file is found, returns a dict of the contents.
+    Otherwise, raises a FileNotFoundError."""
+    try:
+        if (len(argv) == 1):
+            read_file = open("configs.json", "r")
+        else:
+            read_file =  open(argv[1], "r")
+
+        return json.load(read_file)
+
+    except FileNotFoundError:
+        raise
+
+
+def quick_scan():
+    # Parse configs file
+    configs = parse_json(sys.argv)
+
+    # Setup xBee device
+    xbee = setup_xbee(configs)
+
+    # Start autonomy thread
+    autonomy(configs, xbee)
+
+
+if __name__ == "__main__":
+    quick_scan()
