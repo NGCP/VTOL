a8a09ccc68893afaa0e9eb3bec9ef3c80835ba5c
nmcobb <miles.cobb@gmail.com>
2019-04-18 11:58:41 -0700

    control

--- examples/gps_denied.py
+++ examples/gps_denied.py
@@ -5,6 +5,8 @@ import math
 import numpy as np
 from os import listdir
 import cProfile, pstats, StringIO
+from math import cos, sin, radians
+from time import time, sleep
 
 def main():
     sitl = dronekit_sitl.start_default(lat=35.328423, lon=-120.752505) # EFR location
@@ -118,13 +120,11 @@ def cv_simulation():
 def euclidean_distance(x1, y1, x2, y2):
     return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
 
-def change_yaw(theta):
-    # TODO
-    time.sleep(1)
+def change_yaw(vehicle, theta):
+    set_attitude(vehicle, yaw_angle=theta + vehicle.heading, duration=3)
 
-def move_forward():
-    # TODO
-    time.sleep(1)
+def move_forward(vehicle, durration):
+    set_attitude(vehicle, pitch_angle=-15, yaw_rate=0, use_yaw_rate=True, duration=durration)
 
 # Commands drone to take off by arming vehicle and flying to altitude
 def takeoff(vehicle, altitude):
@@ -159,6 +159,78 @@ def land(vehicle):
 
     print("Closing vehicle object")
     vehicle.close()
+
+
+def to_quaternion(roll = 0.0, pitch = 0.0, yaw = 0.0):
+    """
+    Convert degrees to quaternions
+    """
+    t0 = cos(radians(yaw * 0.5))
+    t1 = sin(radians(yaw * 0.5))
+    t2 = cos(radians(roll * 0.5))
+    t3 = sin(radians(roll * 0.5))
+    t4 = cos(radians(pitch * 0.5))
+    t5 = sin(radians(pitch * 0.5))
+
+    w = t0 * t2 * t4 + t1 * t3 * t5
+    x = t0 * t3 * t4 - t1 * t2 * t5
+    y = t0 * t2 * t5 + t1 * t3 * t4
+    z = t1 * t2 * t4 - t0 * t3 * t5
+
+    return [w, x, y, z]
+
+def send_attitude_target(vehicle, roll_angle = 0.0, pitch_angle = 0.0,
+                         yaw_angle = None, yaw_rate = 0.0, use_yaw_rate = False,
+                         thrust = 0.5):
+    """
+    use_yaw_rate: the yaw can be controlled using yaw_angle OR yaw_rate.
+                  When one is used, the other is ignored by Ardupilot.
+    thrust: 0 <= thrust <= 1, as a fraction of maximum vertical thrust.
+            Note that as of Copter 3.5, thrust = 0.5 triggers a special case in
+            the code for maintaining current altitude.
+    """
+    if yaw_angle is None:
+        # this value may be unused by the vehicle, depending on use_yaw_rate
+        yaw_angle = vehicle.attitude.yaw
+    # Thrust >  0.5: Ascend
+    # Thrust == 0.5: Hold the altitude
+    # Thrust <  0.5: Descend
+    msg = vehicle.message_factory.set_attitude_target_encode(
+        0, # time_boot_ms
+        1, # Target system
+        1, # Target component
+        0b00000000 if use_yaw_rate else 0b00000100,
+        to_quaternion(roll_angle, pitch_angle, yaw_angle), # Quaternion
+        0, # Body roll rate in radian
+        0, # Body pitch rate in radian
+        radians(yaw_rate), # Body yaw rate in radian/second
+        thrust  # Thrust
+    )
+    vehicle.send_mavlink(msg)
+
+def set_attitude(vehicle, roll_angle = 0.0, pitch_angle = 0.0,
+                 yaw_angle = None, yaw_rate = 0.0, use_yaw_rate = False,
+                 thrust = 0.5, duration = 0):
+    """
+    Note that from AC3.3 the message should be re-sent more often than every
+    second, as an ATTITUDE_TARGET order has a timeout of 1s.
+    In AC3.2.1 and earlier the specified attitude persists until it is canceled.
+    The code below should work on either version.
+    Sending the message multiple times is the recommended way.
+    """
+    send_attitude_target(vehicle, roll_angle, pitch_angle,
+                         yaw_angle, yaw_rate, use_yaw_rate,
+                         thrust)
+    start = time()
+    while time() - start < duration:
+        send_attitude_target(vehicle, roll_angle, pitch_angle,
+                             yaw_angle, yaw_rate, use_yaw_rate,
+                             thrust)
+        sleep(0.1)
+    # Reset attitude, or it will persist for 1s more due to the timeout
+    send_attitude_target(vehicle, 0, 0,
+                         0, 0, True,
+                         thrust)
 
 if __name__ == "__main__":
     pr = cProfile.Profile()
