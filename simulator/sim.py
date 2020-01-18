a812c512b0d25d8471e80d794c0da9e1a18421c9
nmcobb <miles.cobb@gmail.com>
2020-01-18 10:59:55 -0800

    better simulation

---
+++ simulator/sim.py
@@ -0,0 +1,23 @@
+'''Runs simulator using python'''
+
+import os
+from time import sleep
+from dronekit_sitl import SITL
+SIM = {}
+BIN = os.getenv("SITL_BIN")
+DO_DOWNLOAD = True
+if BIN is not None:
+    DO_DOWNLOAD = False
+    SIM["path"] = BIN
+    DEFAULTS = os.getenv("SITL_DEFAULTS_FILEPATH")
+    if DEFAULTS is not None:
+        SIM["defaults_filepath"] = DEFAULTS
+SIM = SITL(**SIM)
+if DO_DOWNLOAD:
+    SIM.download('copter', '3.3', verbose=False)
+
+SIM.launch(['--model', 'quad', ], await_ready=True, restart=False)
+print("Simulator lanched")
+
+while True:
+    sleep(5)
--- src/main.py
+++ src/main.py
@@ -7,22 +7,7 @@ from quad import QUAD
 
 def main(configs):
     '''Configure vtol and ready for mission'''
-    args = {}
-    binary = os.getenv("SITL_BINARY")
-    do_download = True
-    if binary is not None:
-        do_download = False
-        args["path"] = binary
-        defaults = os.getenv("SITL_DEFAULTS_FILEPATH")
-        if defaults is not None:
-            args["defaults_filepath"] = defaults
-    sitl = SITL(**args)
-    if do_download:
-        sitl.download('copter', '3.3', verbose=True)
-
-    sitl.launch(['--model', 'quad', ], await_ready=True, restart=False)
     vehicle = setup_vehicle(configs, QUAD)
-    print(vehicle.location.global_relative_frame.alt)
 
 if __name__ == '__main__':
     with open('configs.json', 'r') as data:
